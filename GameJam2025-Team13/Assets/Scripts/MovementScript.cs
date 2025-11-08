fb64fa9d06dd4cf00557db5cf668512e39497805
Sean <robertelston@lcad.edu>
2025-11-07 21:10:49 -0800

    added new scene stuff

--- GameJam2025-Team13/Assets/Scripts/MovementScript.cs
+++ GameJam2025-Team13/Assets/Scripts/MovementScript.cs
@@ -8,7 +8,9 @@ public class MovementScript : MonoBehaviour
 
     public float Speed;
 
-    public Transfom Cam;
+    public Transform Cam;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,24 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float Horizontal = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        float Vertical = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+
+        Vector3 Movement = Cam.transform.right * Horizontal + Cam.transform.forward * Vertical;
+        // Movement.y = 0f;
+
+
+        Controller.Move(Movement);
+
+        // if (Movement.magnitude != 0f)
+        // {
+        //     transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Cam.GetComponent<CameraMove>().sensitivity * Time.deltaTime);
+
+        //     Quaternion CamRotation = Cam.rotation;
+        //     CamRotation.x = 0f;
+        //     CamRotation.z = 0f;
+
+        //     transform.rotation = Quaternion.Lerp(transform.rotation, CamRotation, 0.1f);
+        // }
     }
 }
