f8f332f2bec4f8ef4b6be91a968d85096d2ffe27
Sean <robertelston@lcad.edu>
2025-11-08 23:47:07 -0800

    11:47 commit

---
+++ GameJam2025-Team13/Assets/Scripts/UI_Handler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI_Handler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        
+    }
+}
